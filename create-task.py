bf04a8f9830fdadf6d9b279e699bc403f14dab13
August Shultz <augustshultz@gmail.com>
2021-03-14 21:27:36 -0500

    Pass in task content to the script

--- create-task.py
+++ create-task.py
@@ -2,14 +2,20 @@ import requests
 import uuid
 import json
 import config
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('task_name', help="The name of the task to be created.")
+
+arguments = parser.parse_args()
 
 requests.post(
-    "https://api.todoist.com/rest/v1/tasks",
+    'https://api.todoist.com/rest/v1/tasks',
     data=json.dumps({
-        "content": "Tester",
+        'content': arguments.task_name,
     }),
     headers={
-        "Content-Type": "application/json",
-        "X-Request-Id": str(uuid.uuid4()),
+        'Content-Type': 'application/json',
+        'X-Request-Id': str(uuid.uuid4()),
         'Authorization': f'Bearer {config.api_token}'
     }).json()
