c0c3da88f7b34a6e9dcc0942d5bbe4f62a364588
August Shultz <augustshultz@gmail.com>
2021-03-15 19:10:06 -0500

    Print out created task response

--- create-task.py
+++ create-task.py
@@ -9,7 +9,7 @@ parser.add_argument('task_name', help="The name of the task to be created.")
 
 arguments = parser.parse_args()
 
-requests.post(
+response = requests.post(
     'https://api.todoist.com/rest/v1/tasks',
     data=json.dumps({
         'content': arguments.task_name,
@@ -19,3 +19,5 @@ requests.post(
         'X-Request-Id': str(uuid.uuid4()),
         'Authorization': f'Bearer {config.api_token}'
     }).json()
+
+print(response)
