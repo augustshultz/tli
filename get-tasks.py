7cfbad1afe50b94b318155b9b23c737a81d440fe
August Shultz <augustshultz@gmail.com>
2021-03-17 20:01:22 -0500

    Add get-tasks functionality to get tasks for a single project

---
+++ get-tasks.py
@@ -0,0 +1,22 @@
+import argparse
+import requests
+import config
+from task import Task
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--project', '-p')
+
+arguments = parser.parse_args()
+
+response = requests.get(
+    'https://api.todoist.com/rest/v1/tasks',
+    params={
+        'project_id': f'{arguments.project}'
+    },
+    headers={
+        'Authorization': f'Bearer {config.api_token}'
+    }).json()
+
+tasks = [Task(**kwargs) for kwargs in response]
+for task in tasks:
+    print(task)
