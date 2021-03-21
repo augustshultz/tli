85257ac4526cd90b1136ea4e441dd36866e45f17
August Shultz <augustshultz@gmail.com>
2021-03-21 07:53:50 -0500

    Support deletion of multiple tasks

--- delete-task.py
+++ delete-task.py
@@ -2,17 +2,26 @@ import argparse
 import requests
 import config
 
-parser = argparse.ArgumentParser()
-parser.add_argument('task_id', help="The id of the task to be deleted.")
 
-arguments = parser.parse_args()
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('task_ids', nargs='+', help='The id(s) of the task(s) to be deleted.')
+    arguments = parser.parse_args()
+    task_ids = arguments.task_ids
+    for task_id in task_ids:
+        delete_task(task_id=task_id)
 
-response = requests.delete(f"https://api.todoist.com/rest/v1/tasks/{arguments.task_id}",
-                           headers={
-                               "Authorization": f"Bearer {config.api_token}"
-                           })
 
-if response.status_code == 204:
-    print('Successfully deleted task.')
-else:
-    print('Something went wrong')
+def delete_task(*, task_id):
+    url = f'https://api.todoist.com/rest/v1/tasks/{task_id}'
+    headers = {'Authorization': f'Bearer {config.api_token}'}
+    response = requests.delete(url, headers=headers)
+
+    if response.status_code == 204:
+        print('Successfully deleted task.')
+    else:
+        print('Something went wrong')
+
+
+if __name__ == '__main__':
+    parse_arguments()
