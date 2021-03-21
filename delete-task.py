a3fd8c6eff38baefee4cba8e05b2ea9966b0f051
August Shultz <augustshultz@gmail.com>
2021-03-21 08:02:51 -0500

    Use new url utility to generate delete task url

--- delete-task.py
+++ delete-task.py
@@ -1,6 +1,7 @@
 import argparse
 import requests
 import config
+from urls import delete_task_url
 
 
 def parse_arguments():
@@ -13,7 +14,7 @@ def parse_arguments():
 
 
 def delete_task(*, task_id):
-    url = f'https://api.todoist.com/rest/v1/tasks/{task_id}'
+    url = delete_task_url(task_id=task_id)
     headers = {'Authorization': f'Bearer {config.api_token}'}
     response = requests.delete(url, headers=headers)
 
