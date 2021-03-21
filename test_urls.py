d9cf55e93244ab7bfb4f462fc63a4edb2918467b
August Shultz <augustshultz@gmail.com>
2021-03-21 08:00:40 -0500

    Create utility to generate urls

---
+++ test_urls.py
@@ -0,0 +1,9 @@
+import pytest
+
+from urls import delete_task_url
+
+
+def test_delete_task_url():
+    task_id = '4677084901'
+    expected = f'https://api.todoist.com/rest/v1/tasks/{task_id}'
+    assert delete_task_url(task_id=task_id) == expected
---
+++ urls.py
@@ -0,0 +1,2 @@
+def delete_task_url(*, task_id):
+    return f'https://api.todoist.com/rest/v1/tasks/{task_id}'
