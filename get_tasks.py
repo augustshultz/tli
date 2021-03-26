e093772696fe4413fa0baef8f111def15c0f38e1
August Shultz <augustshultz@gmail.com>
2021-03-26 07:04:38 -0500

    refactor: put get_tasks functionality into a function

--- get_tasks.py
+++ get_tasks.py
@@ -3,20 +3,29 @@ import requests
 import config
 from models import Task
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--project', '-p')
 
-arguments = parser.parse_args()
+def get_tasks_from_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--project_id', '-p')
+    arguments = parser.parse_args()
+    get_tasks(project_id=arguments.project_id)
 
-response = requests.get(
-    'https://api.todoist.com/rest/v1/tasks',
-    params={
-        'project_id': f'{arguments.project}'
-    },
-    headers={
-        'Authorization': f'Bearer {config.api_token}'
-    }).json()
 
-tasks = [Task(**kwargs) for kwargs in response]
-for task in tasks:
-    print(task)
+def get_tasks(*, project_id):
+    params = {}
+    if project_id:
+        params['project_id'] = project_id
+
+    response = requests.get(
+        'https://api.todoist.com/rest/v1/tasks',
+        params=params,
+        headers={
+            'Authorization': f'Bearer {config.api_token}'
+        }).json()
+    tasks = [Task(**kwargs) for kwargs in response]
+    for task in tasks:
+        print(task)
+
+
+if __name__ == '__main__':
+    get_tasks_from_arguments()
