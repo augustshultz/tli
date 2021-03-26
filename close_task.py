9c07eeefdbd384e098291c6ece9fd45df8670123
August Shultz <augustshultz@gmail.com>
2021-03-26 07:14:35 -0500

    refactor: move close task logic into a function

--- close_task.py
+++ close_task.py
@@ -2,17 +2,23 @@ import argparse
 import requests
 import config
 
-parser = argparse.ArgumentParser()
-parser.add_argument('task_id', help="The id of the task to be deleted.")
 
-arguments = parser.parse_args()
+def close_task():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('task_id', help="The id of the task to be deleted.")
 
-response = requests.post(f"https://api.todoist.com/rest/v1/tasks/{arguments.task_id}/close",
-                         headers={
-                             "Authorization": f"Bearer {config.api_token}"
-                         })
+    arguments = parser.parse_args()
 
-if response.status_code == 204:
-    print('Successfully closed the task.')
-else:
-    print('Something went wrong')
+    response = requests.post(f"https://api.todoist.com/rest/v1/tasks/{arguments.task_id}/close",
+                             headers={
+                                 "Authorization": f"Bearer {config.api_token}"
+                             })
+
+    if response.status_code == 204:
+        print('Successfully closed the task.')
+    else:
+        print('Something went wrong')
+
+
+if __name__ == '__main__':
+    close_task()
