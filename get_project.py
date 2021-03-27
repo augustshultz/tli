79750ccbd3273724faae2b1cc87fa5251ae7b8dd
August Shultz <augustshultz@gmail.com>
2021-03-27 17:15:17 -0500

    refactor: clean up get_project

--- get_project.py
+++ get_project.py
@@ -5,15 +5,17 @@ import requests
 import config
 from urls import get_project_url
 
-parser = argparse.ArgumentParser()
-parser.add_argument('project_id')
 
-arguments = parser.parse_args()
+def get_project(project_id):
+    url = get_project_url(project_id=project_id)
+    headers = {'Authorization': f'Bearer {config.api_token}'}
+    response = requests.get(url, headers=headers).json()
+    print(json.dumps(response, indent=1))
 
-response = requests.get(
-    get_project_url(project_id=arguments.project_id),
-    headers={
-        'Authorization': f'Bearer {config.api_token}'
-    }).json()
 
-print(json.dumps(response, indent=1))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('project_id')
+
+    arguments = parser.parse_args()
+    get_project(project_id=arguments.project_id)
