cef26d2cc5ca2dbded4e66d422b7d100c7edf0f1
August Shultz <augustshultz@gmail.com>
2021-03-28 14:15:55 -0500

    refactor: return Task from create_task function

--- create_task.py
+++ create_task.py
@@ -4,8 +4,10 @@ import json
 import config
 import argparse
 
+from models import Task
 
-def create_task(*, task_name):
+
+def create_task(*, task_name: str) -> Task:
     headers = {
         'Content-Type': 'application/json',
         'X-Request-Id': str(uuid.uuid4()),
@@ -15,12 +17,13 @@ def create_task(*, task_name):
         'content': task_name,
     })
     url = 'https://api.todoist.com/rest/v1/tasks'
-    response = requests.post(url, data=data, headers=headers).json()
-    print(json.dumps(response, indent=1))
+    response = requests.post(url, data=data, headers=headers)
+    return Task(**response.json())
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('task_name', help="The name of the task to be created.")
     arguments = parser.parse_args()
-    create_task(task_name=arguments.task_name)
+    task = create_task(task_name=arguments.task_name)
+    print(task)
