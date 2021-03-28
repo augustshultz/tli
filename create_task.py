dc285cf931b0a7a9cd4a24ab2aeee803803c5dcb
August Shultz <augustshultz@gmail.com>
2021-03-28 14:33:25 -0500

    feature: set due value for new task

--- create_task.py
+++ create_task.py
@@ -6,7 +6,7 @@ import argparse
 from models import Task
 
 
-def create_task(*, task_name: str) -> Task:
+def create_task(*, task_name: str, due: str = None) -> Task:
     headers = {
         'Content-Type': 'application/json',
         'X-Request-Id': str(uuid.uuid4()),
@@ -15,6 +15,8 @@ def create_task(*, task_name: str) -> Task:
     data = {
         'content': task_name,
     }
+    if due:
+        data['due_string'] = due
     url = 'https://api.todoist.com/rest/v1/tasks'
     response = requests.post(url, json=data, headers=headers)
     if response.status_code != 200:
@@ -24,7 +26,8 @@ def create_task(*, task_name: str) -> Task:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('task_name', help="The name of the task to be created.")
+    parser.add_argument('task_name', help="The name of the task.")
+    parser.add_argument('--due', '-d', help="The due value for the task.")
     arguments = parser.parse_args()
-    task = create_task(task_name=arguments.task_name)
+    task = create_task(task_name=arguments.task_name, due=arguments.due)
     print(task)
