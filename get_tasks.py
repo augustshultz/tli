aa3f199fedac8912c6798d27ea9710a44d0fb5fa
August Shultz <augustshultz@gmail.com>
2021-04-14 18:29:02 -0500

    feature: add flag to fetch inbox tasks

--- get_tasks.py
+++ get_tasks.py
@@ -3,6 +3,7 @@ import requests
 from typing import List
 
 import config
+from get_all_projects import get_all_projects
 from models import Task
 
 
@@ -10,7 +11,10 @@ def get_tasks_from_arguments() -> List[Task]:
     parser = argparse.ArgumentParser()
     parser.add_argument('--project_id', '-p')
     parser.add_argument('--filter', '-f')
+    parser.add_argument('--inbox', '-i', action='store_true')
     arguments = parser.parse_args()
+    if arguments.inbox:
+        return get_inbox_tasks()
     return get_tasks(
         project_id=arguments.project_id,
         tasks_filter=arguments.filter
@@ -33,6 +37,12 @@ def get_tasks(*, project_id=None, tasks_filter: str = None) -> List[Task]:
     if response.status_code != 200:
         raise Exception(f'{response.status_code}: {response.reason}')
     return [Task(**kwargs) for kwargs in response.json()]
+
+
+def get_inbox_tasks() -> List[Task]:
+    projects = get_all_projects()
+    inbox, *_ = filter(lambda project: project.inbox, projects)
+    return get_tasks(project_id=inbox.project_id)
 
 
 if __name__ == '__main__':
