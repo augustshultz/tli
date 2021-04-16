8847e3e249a1f43b9267747ea48985f677fa14ed
August Shultz <augustshultz@gmail.com>
2021-04-16 14:47:14 -0500

    Throw error when failing to grab project

--- get_project.py
+++ get_project.py
@@ -10,8 +10,10 @@ from urls import get_project_url
 def get_project(project_id: str) -> Project:
     url = get_project_url(project_id=project_id)
     headers = {'Authorization': f'Bearer {config.api_token}'}
-    response = requests.get(url, headers=headers).json()
-    return Project(**response)
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f'Error fetching project: {project_id}')
+    return Project(**response.json())
 
 
 if __name__ == '__main__':
@@ -19,5 +21,8 @@ if __name__ == '__main__':
     parser.add_argument('project_id')
 
     arguments = parser.parse_args()
-    project = get_project(project_id=arguments.project_id)
-    print(project)
+    try:
+        project = get_project(project_id=arguments.project_id)
+        print(project)
+    except Exception as e:
+        print(e)
