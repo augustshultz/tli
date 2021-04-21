efbd7187a484ee97db7d8b182951740e5453934c
August Shultz <augustshultz@gmail.com>
2021-04-21 07:50:07 -0500

    refactor: start moving get task interaction into task.py

---
+++ task.py
@@ -0,0 +1,23 @@
+import argparse
+import requests
+import config
+from models import Task
+
+
+def get_task(task_id: str) -> Task:
+    url = f"https://api.todoist.com/rest/v1/tasks/{task_id}"
+    headers = {'Authorization': f'Bearer {config.api_token}'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f'Failed to get task: {task_id}')
+
+    return Task(**response.json())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', help='The id of the task to get.')
+    arguments = parser.parse_args()
+    task = get_task(task_id=arguments.id)
+    print(task)
