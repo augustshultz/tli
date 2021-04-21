97054787f75b2a11aa02e685d9c0a385e4b56398
August Shultz <augustshultz@gmail.com>
2021-04-21 07:52:39 -0500

    chore: gracefully indicate no task found for given task id

--- task.py
+++ task.py
@@ -18,6 +18,11 @@ def get_task(task_id: str) -> Task:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('id', help='The id of the task to get.')
+
     arguments = parser.parse_args()
-    task = get_task(task_id=arguments.id)
-    print(task)
+
+    try:
+        task = get_task(task_id=arguments.id)
+        print(task)
+    except Exception as e:
+        print(e)
