ed353c042d286b40607e6453380e3b67c13a7bcb
August Shultz <augustshultz@gmail.com>
2021-04-22 17:21:17 -0500

    feature: add deletion ability to task.py

--- task.py
+++ task.py
@@ -1,15 +1,19 @@
 import argparse
 
+from delete_task import delete_task
 from get_task import get_task
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('id', help='The id of the task to get.')
+    parser.add_argument('--delete', '-d', help='The id of the task to delete.', action='store_true')
 
     arguments = parser.parse_args()
 
     try:
-        task = get_task(task_id=arguments.id)
-        print(task)
+        if arguments.delete:
+            delete_task(task_id=arguments.id)
+        else:
+            print(get_task(task_id=arguments.id))
     except Exception as e:
         print(e)
