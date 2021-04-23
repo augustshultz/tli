d3d3c17f3c90431869a3c36fd49ce7aa555bd349
August Shultz <augustshultz@gmail.com>
2021-04-23 16:22:53 -0500

    feature: add script to a close task

--- close_task.py
+++ close_task.py
@@ -1,30 +1,26 @@
 import argparse
+
 import requests
+
 import config
-
-
-def close_task_with_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('task_id', help='The id of the task to be deleted.')
-
-    arguments = parser.parse_args()
-    close_task(task_id=arguments.task_id)
+from urls import close_task_url
 
 
 def close_task(*, task_id):
-    url = f'https://api.todoist.com/rest/v1/tasks/{task_id}/close'
-    response = requests.post(
-        url,
-        headers={
-            'Authorization': f'Bearer {config.api_token}'
-        }
-    )
+    url = close_task_url(task_id=task_id)
+    headers = {'Authorization': f'Bearer {config.api_token}'}
+    response = requests.post(url, headers=headers)
 
-    if response.status_code == 204:
-        print('Successfully closed the task.')
-    else:
-        print('Something went wrong')
+    if response.status_code != 204:
+        raise Exception(f'Failed to close task: {task_id}')
+    print(f'Closed task: {task_id}')
 
 
 if __name__ == '__main__':
-    close_task_with_arguments()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('task_id', help='The id of the task to be closed.')
+    arguments = parser.parse_args()
+    try:
+        close_task(task_id=arguments.task_id)
+    except Exception as e:
+        print(str(e))
