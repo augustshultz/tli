54370a4ede773ac1250128f0139b2b01ffccd215
August Shultz <augustshultz@gmail.com>
2023-09-12 21:15:14 -0500

    chore: Pulls out main endpoint in the url file

--- tli/urls.py
+++ tli/urls.py
@@ -1,10 +1,13 @@
+_endpoint: str = 'https://api.todoist.com/rest/v2/'
+
+
 def delete_task_url(*, task_id):
-    return f"https://api.todoist.com/rest/v2/tasks/{task_id}"
+    return f"{_endpoint}tasks/{task_id}"
 
 
 def close_task_url(*, task_id):
-    return f"https://api.todoist.com/rest/v2/tasks/{task_id}/close"
+    return f"{_endpoint}tasks/{task_id}/close"
 
 
 def get_project_url(*, project_id):
-    return f"https://api.todoist.com/rest/v2/projects/{project_id}"
+    return f"{_endpoint}projects/{project_id}"
