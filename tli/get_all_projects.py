9e30b1dabf5d601e3be5ea47f060e4e7fd839bfd
August Shultz <augustshultz@gmail.com>
2023-09-13 19:35:55 -0500

    refactor: Uses a list literal instead of a map

--- tli/get_all_projects.py
+++ tli/get_all_projects.py
@@ -5,11 +5,11 @@ from api import Api
 from models import Project
 
 
-def get_all_projects() -> Iterator[Project]:
-    status_code, response = Api(api_token=config.api_token).get(path="projects")
+def get_all_projects() -> list[Project]:
+    status_code, results = Api(api_token=config.api_token).get(path="projects")
     if status_code != 200:
         raise Exception("Error fetching all projects.")
-    return map(lambda kwargs: Project(**kwargs), response)
+    return [Project(**result) for result in results]
 
 
 if __name__ == "__main__":
